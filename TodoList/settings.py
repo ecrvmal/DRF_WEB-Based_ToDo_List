ba0ba71c726f10a14dc671e4144559e065cadb61
ecrvmal <ecrvmal@yandex.ru>
2023-09-25 18:51:24 +0300

    Working project

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -106,11 +106,21 @@ WSGI_APPLICATION = 'TodoList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolist',
+        'USER': 'dante1',
+        'PASSWORD': 'dante123456',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,     }
 }
 
 # Password validation
